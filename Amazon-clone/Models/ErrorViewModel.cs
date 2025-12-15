312b69aec953bfce7f4c0e62513e8013dc50e9ee
Danya <Danya@DANYA-PC83>
2025-12-15 14:14:22 +0200

    Add project files.

---
+++ Amazon-clone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Amazon_clone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
