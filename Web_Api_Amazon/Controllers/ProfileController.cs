9b4516d353cc488cfcff64885336729ed788ad05
vikaYurchuk <vikajurchyk@gmail.com>
2026-03-03 23:24:14 +0200

    Account profile and security added

---
+++ Web_Api_Amazon/Controllers/ProfileController.cs
@@ -0,0 +1,38 @@
+using Azure.Core;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Web_Api_Amazon.Entities;
+
+public class ProfileController : Controller
+{
+    private readonly UserManager<User> _userManager;
+
+    public ProfileController(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Security()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null) return RedirectToAction("Entry");
+
+        var session = new
+        {
+            DeviceName = "This device",
+            BrowserName = Request.Headers["User-Agent"].ToString(),
+            IpAddress = HttpContext.Connection.RemoteIpAddress?.ToString(),
+            IsCurrent = true
+        };
+
+        ViewBag.Sessions = new[] { session };
+        ViewBag.CurrentToken = "current";
+        return View();
+    }
+    public IActionResult Index()
+    {
+        ViewData["ActivePage"] = "Profile";
+        return View();
+    }
+}
